63ac21fe66ce6183b1bda968b8770c07a011951b
Malayaacx01 <111335173+Malayaacx01@users.noreply.github.com>
2024-01-26 11:48:40 +0800

    Update setupku.sh

--- setupku.sh
+++ setupku.sh
@@ -132,7 +132,7 @@ clear
 wget https://raw.githubusercontent.com/Malayaacx01/Autoscript-vpn/main/ssh-vpn.sh && chmod +x ssh-vpn.sh && ./ssh-vpn.sh
 sleep 2
 clear
-wget https://raw.githubusercontent.com/Azigaming404/Autoscript-by-azi/main/nginx-ssl.sh && chmod +x nginx-ssl.sh && ./nginx-ssl.sh
+wget https://raw.githubusercontent.com/Malayaacx01/Autoscript-vpn/main/nginx-ssl.sh && chmod +x nginx-ssl.sh && ./nginx-ssl.sh
 
 
 #install ssh ovpn
