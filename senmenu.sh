dd023ff05c19fdb6ec56a6fc3568c63313d0a73a
Malayaacx01 <111335173+Malayaacx01@users.noreply.github.com>
2024-01-26 13:33:58 +0800

    Update senmenu.sh

--- senmenu.sh
+++ senmenu.sh
@@ -3,8 +3,8 @@
 echo -e "${GREEN}Config Data${NC}"
 
 wget -q -O /usr/bin/menu "https://raw.githubusercontent.com/Malayaacx01/Autoscript-vpn/main/menu.sh"
-wget -q -O /usr/bin/add-ws "https://raw.githubusercontent.com/Azigaming404/Autoscript-by-azi/main/add-ws.sh"
-wget -q -O /usr/bin/add-ssws "https://raw.githubusercontent.com/Azigaming404/Autoscript-by-azi/main/add-ssws.sh"
+wget -q -O /usr/bin/add-ws "https://raw.githubusercontent.com/Malayaacx01/Autoscript-vpn/main/vmess/add-ws.sh"
+wget -q -O /usr/bin/add-ssws "https://raw.githubusercontent.com/Malayaacx01/Autoscript-vpn/main/ssws/add-ssws.sh"
 #wget -q -O /usr/bin/add-socks "https://raw.githubusercontent.com/Agunxzzz/XrayCol/main/minacantik/add-socks.sh"
 wget -q -O /usr/bin/add-vless "https://raw.githubusercontent.com/Azigaming404/Autoscript-by-azi/main/add-vless.sh"
 wget -q -O /usr/bin/add-tr "https://raw.githubusercontent.com/Azigaming404/Autoscript-by-azi/main/add-tr.sh"
